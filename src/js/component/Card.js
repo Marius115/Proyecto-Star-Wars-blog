685a4b81baced713eaefc08e9ca5d8e83c997719
Marius115 <cascaranomeister@gmail.com>
2022-06-20 07:46:37 +0000

    Estilizado v2

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -6,11 +6,10 @@ import { Link } from "react-router-dom";
 export const Card = ({item,resource}) => {
     return(
 	<div className="tab-pane ps-2 pb-1 pt-2 pe-2" role="tabpanel" aria-labelledby="profile-tab" tabIndex="0">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card border-warning" style={{width: "18rem"}}>
   			<img src={`https://starwars-visualguide.com/assets/img/${resource === "people" ? "characters" : resource }/${item.uid}.jpg`} className="card-img-top" alt="..." />
- 			 <div className="card-body">
-   			 <h5 className="card-title">{item.name}</h5>
-   			 <p className="card-text"> Lorem Impsum </p>
+ 			 <div className="card-body bg-dark">
+   			 <h5 className="card-title text-warning">{item.name}</h5>
     		 <Link to={`/single/${resource}/${item.uid}`}  className="btn btn-primary justify-content-center align-middle">Go somewhere</Link>
   			 </div>
 		</div>
